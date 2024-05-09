b2b0d7e872d9227d2662340fb26306d4b9a70d87
Anderson Correia <anderson_souza33@outlook.com.br>
2024-05-09 16:02:38 -0300

    feat: object to create movie

--- src/module/movie/dto/create-movie.dto.ts
+++ src/module/movie/dto/create-movie.dto.ts
@@ -1,1 +1,34 @@
-export class CreateMovieDto {}
+import { IsDateString, IsEnum, IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
+import { EnumCategory } from "../enums/cateegory.enum";
+
+export class CreateMovieDto {
+
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    writer: string;
+
+    @IsNotEmpty()
+    @IsString()
+    plot: string;
+
+    @IsNotEmpty()
+    @IsEnum(EnumCategory)
+    category: EnumCategory;
+
+    @IsNotEmpty()
+    @IsString()
+    productionCompany: string;
+
+    @IsNotEmpty()
+    @IsDateString()
+    releaseDate: Date;
+
+    @IsNotEmpty()
+    @IsNumber({maxDecimalPlaces: 0})
+    @Min(60)
+    runningTime: number;
+}
---
+++ src/module/movie/enums/cateegory.enum.ts
@@ -0,0 +1,9 @@
+export enum EnumCategory {
+    ACTION = `Action`,
+    ADVENTURE = `Adventure`,
+    COMEDY = `Comedy`,
+    DRAMA = `Drama`,
+    HORROR = `Horror`,
+    ROMANCE = `Romance`,
+    SCIENCE_FICTION = `Science Fiction`
+}

a511abd8ea43fc3c548ebe56111b59eafd0b7390
Anderson815 <anderson_souza33@outlook.com.br>
2024-05-10 19:27:13 -0300

    refactor: rule of the years old

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,9 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserGatewayInterface } from './gateways/user-gateway-interface';
 import { hashSync, compareSync } from "bcrypt";
 import { plainToClass } from 'class-transformer';
 import { UserResponse } from './dto/user-response.dto';
+import * as moment from 'moment';
 
 @Injectable()
 export class UserService {
@@ -13,6 +14,14 @@ export class UserService {
     ){}
 
     async create(createUserDto: CreateUserDto){            
+        const yearOldMin = 10;
+
+        const dateMomentBirth = moment(createUserDto.dateBirth);
+        const dateMomentNow = moment(new Date);
+        
+        if(dateMomentNow.diff(dateMomentBirth, 'year') <= yearOldMin)
+            throw new BadRequestException(`You must be more or equal than 10 years old`);
+
         createUserDto.password = hashSync(createUserDto.password, Number(process.env.BCRYPT_SALT));
         const newUser = await this.userGateway.create(createUserDto);
         const userToResponse = plainToClass(UserResponse, newUser);

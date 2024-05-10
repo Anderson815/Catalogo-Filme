63af6a9a91da1e8eabe205abd5095ed62b892004
Anderson Correia <anderson_souza33@outlook.com.br>
2024-05-10 11:35:40 -0300

    build: docker config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MovieModule } from './module/movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    MovieModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'root',
+      password: 'pass',
+      database: 'movie_db',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
